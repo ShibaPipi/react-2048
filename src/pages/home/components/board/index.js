c8d24c99550e931e7695ff9a68dc60298afd36b6
ShibaPipi <906262260@qq.com>
2020-01-17 00:17:31 +0800

    project init

--- src/pages/home/components/board/index.js
+++ src/pages/home/components/board/index.js
@@ -10,18 +10,71 @@ class Grid extends Component {
     }
   }
 
+  handleColor = (value) => {
+    return value <= 4 ? '#776e65' : 'white';
+  };
+
+  handleBackgroundColor = (value) => {
+    switch (value) {
+      case 2:
+        return '#eee4da';
+      case 4:
+        return '#ede0c8';
+      case 8:
+        return '#f2b179';
+      case 16:
+        return '#f59563';
+      case 32:
+        return '#f67c5f';
+      case 64:
+        return '#f65e3b';
+      case 128:
+        return '#edcf72';
+      case 256:
+        return '#edcc61';
+      case 512:
+        return '#9c0';
+      case 1024:
+        return '#33b5e5';
+      case 2048:
+        return '#09c';
+      case 4096:
+        return '#a6c';
+      case 8192:
+        return '#93c';
+      default:
+        return 'black';
+    }
+  };
+
   render() {
     console.log(this.state.cellNumber)
 
     return (
       <div className={styles.board}>
-        {this.state.cellNumber.keys.map(value =>
-          <div key={value} className={styles.cell}
-               style={{ 'top': `${20 + 120 * parseInt(value / 4)}px`, 'left': `${20 + 120 * (value % 4)}px` }} />
+        {this.state.cellNumber.map((value, index) =>
+          <div key={index} className={styles.cell}
+               style={{ 'top': `${20 + 120 * parseInt(index / 4)}px`, 'left': `${20 + 120 * (index % 4)}px` }}/>
         )}
         {
-          this.state.cellNumber.map(value =>
-            <div key={value} className={styles.cellNumber}>{value}</div>
+          this.state.cellNumber.map((value, index) =>
+            <div key={value} className={styles.cellNumber} style={
+              0 === value
+                ? {
+                  // 'width': '0',0
+                  // 'height': '0',
+                  'top': `${20 + 120 * parseInt(index / 4)}px`,
+                  'left': `${55 + 120 * (index % 4)}px`,
+                }
+                : {
+                  'width': '100px',
+                  'height': '100px',
+                  'top': `${20 + 120 * parseInt(index / 4)}px`,
+                  'left': `${20 + 120 * (index % 4)}px`,
+                  'color': this.handleColor(value),
+                  'background-color': this.handleBackgroundColor(value)
+                }
+            }>{value}</div>
           )
         }
       </div>
